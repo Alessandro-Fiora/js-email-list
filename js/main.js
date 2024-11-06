4ed766193ff2d7099f62f36c2bd693bac4f6b97e
Alessandro-Fiora <fiora.ale97@gmail.com>
2024-11-06 16:20:28 +0100

    better looks

--- js/main.js
+++ js/main.js
@@ -6,10 +6,11 @@ const printEmails = () => {
   for (let i = 0; i < 10; i++) {
     fetch("https://flynn.boolean.careers/exercises/api/random/mail")
       .then((result) => result.json())
-      .then((data) => {
-        const email = data.response;
-
-        emailList.innerHTML += `<li>${email}</li>`;
+      .then((email) => {
+        emailList.innerHTML += `
+            <li class="list-group-item" >
+              <a href="#">${email.response}</a>
+            </li>`;
       });
   }
 };
