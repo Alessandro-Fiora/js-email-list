b4222962061af7a61d9d8bb3dcb71983ef1f2a71
Alessandro-Fiora <fiora.ale97@gmail.com>
2024-11-06 15:44:59 +0100

    print another 10 emails

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 const emailList = document.getElementById("email-list");
-console.log(emailList);
+const printEmailsButton = document.getElementById("email-print-button");
 
-for (let i = 0; i < 10; i++) {
-  fetch("https://flynn.boolean.careers/exercises/api/random/mail")
-    .then((result) => result.json())
-    .then((data) => {
-      const email = data.response;
+const printEmails = () => {
+  for (let i = 0; i < 10; i++) {
+    fetch("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then((result) => result.json())
+      .then((data) => {
+        const email = data.response;
 
-      emailList.innerHTML += `<li>${email}</li>`;
-    });
-}
+        emailList.innerHTML += `<li>${email}</li>`;
+      });
+  }
+};
+
+printEmails();
+
+printEmailsButton.addEventListener("click", printEmails);
